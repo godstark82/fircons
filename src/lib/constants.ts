ea0bc43fa2245e2f937ad12675d73495e9b6bead
Pankaj sharma <ls8290519977@gmail.com>
2026-07-26 19:56:54 -0700

    admin updated

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,7 @@ export const CONSTANTS = {
     ABBR_SHORT: "IS-FMS",
     YEAR: "2026",
     CONFERENCE_FULL_TITLE: "International Symposium on Frontiers in Mathematical Sciences",
-    CONFERENCE_DATES: "September 17-19, 2026",
+    CONFERENCE_DATES: "September 25-27, 2026",
     COLLAGE_NAME: "G H Raisoni College of Engineering, Nagpur",
     COLLAGE_ADDRESS: "CRPF Gate No. 3, Hingna Road, Digdoh Hills, Nagpur, Maharashtra 440016",
 }
